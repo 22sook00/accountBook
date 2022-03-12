80578e2838293fc7ae9243c584e0bcbe06fc7f54
엔코 이숙영 <sookyoung.lee@enkor.kr>
2022-03-12 15:23:54 +0900

    Create addFormModal

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { increment } from "../../slices/addFormSlice";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../store";
 import Buttons from "../../components/Common/Buttons/Buttons";
 import Lists from "../../components/Lists/Lists";
 import DatePickers from "../../components/Common/DatePickers/DatePickers";
+import AddFormModal from "../../components/Forms/AddFormModal/AddFormModal";
 
 const MainPage = () => {
   const dispatch = useDispatch();
 
-  const handleClickAddBtn = () => {
-    dispatch(increment());
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleClickMoveAddForm = () => {
+    setIsOpen(true);
   };
   return (
     <>
       <DatePickers />
+      <Lists />
       <Buttons
         bottom={"bottom"}
         bgcolor="rgb(177, 189, 220)"
-        onclick={handleClickAddBtn}
+        onclick={handleClickMoveAddForm}
         text={"+"}
       />
-      <Lists />
+
+      {isOpen ? <AddFormModal setIsOpen={setIsOpen} /> : <></>}
     </>
   );
 };
