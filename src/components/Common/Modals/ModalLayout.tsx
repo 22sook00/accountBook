f9e3fa09bcc5a0a5b7379967100739cd5cbc993f
엔코 이숙영 <sookyoung.lee@enkor.kr>
2022-06-01 02:26:12 +0900

    Complete add form mobile ver with tailwind

--- src/components/Common/Modals/ModalLayout.tsx
+++ src/components/Common/Modals/ModalLayout.tsx
@@ -1,6 +1,5 @@
 import { Dialog, Transition } from "@headlessui/react";
 import React, { FC, Fragment, SetStateAction, useCallback } from "react";
-import Buttons from "../Buttons/Buttons";
 import "./modalLayout.css";
 import { XIcon } from "@heroicons/react/outline";
 interface Props {
@@ -14,7 +13,7 @@ const ModalLayout: FC<Props> = ({ isOpen, setIsOpen, children, title }) => {
     setIsOpen(!isOpen);
   }, [isOpen, setIsOpen]);
   return (
-    <section className="bg-white ">
+    <section className="scrollHide">
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={handleCloseBtn}>
           <Transition.Child
@@ -29,14 +28,14 @@ const ModalLayout: FC<Props> = ({ isOpen, setIsOpen, children, title }) => {
             <div className="fixed inset-0 bg-white lg:bg-black lg:opacity-60 px-4" />
           </Transition.Child>
 
-          <div className=" fixed inset-0 overflow-y-auto">
+          <div className="scrollHide fixed inset-0 overflow-y-auto">
             <div className="w-full z-50 fixed top-0 p-4 border-b shadow bg-white">
               <XIcon
                 onClick={handleCloseBtn}
                 className="w-6 h-6 text-text-light font-bold cursor-pointer transition hover:rotate-90"
               />
             </div>
-            <div className="p-4  flex min-h-full items-center justify-center  text-center">
+            <div className="p-6  flex min-h-full items-center justify-center  text-center">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -46,9 +45,11 @@ const ModalLayout: FC<Props> = ({ isOpen, setIsOpen, children, title }) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="mt-[80px] w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle lg:shadow-xl transition-all">
-                  <h3 className="text-sm">{title}</h3>
-                  <>{children}</>
+                <Dialog.Panel className="pt-[60px] w-full max-w-md transform overflow-x-hidden scroll-smooth overflow-y-scroll rounded-2xl bg-white text-left align-middle lg:shadow-xl transition-all">
+                  <div className="scrollHide">
+                    <h3 className="text-sm">{title}</h3>
+                    <>{children}</>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
